b907ed37ed416066aa95361eadef31e28db19ae2
zhenyuanh <huangzy087@gmail.com>
2016-08-24 21:15:45 +0800

    chat_server

--- learning_node/nodeStudyAndUse/d2_chatServer.js
+++ learning_node/nodeStudyAndUse/d2_chatServer.js
@@ -1,14 +1,41 @@
-var net = require('net')
-var chatServer = net.createServer();
-chatServer.on('connection', function(client) {
-	client.write('Hi!\n');
-	client.on('data', function(data){
-		console.log(data.toString())
+var net = require('net');
 
-	})
+var chatServer = net.createServer(),
+	clientList = [];
+
+
+chatServer.on('connection', function(client) {
+	client.name = client.remoteAddress + ':' + client.remotePort;
+	client.write('Hi!\n');
+
+	clientList.push(client);
+
+	client.on('data', function(data){
+		// console.log(data.toString());
+		if (data.length > 0 ) {
+			broadcast(data, client);
+		}
+	});
+
+	client.on('end', function(data){
+		clientList.splice(clientList.indexOf(client), 1);
+	});
+});
+
+function broadcast(message, client) {
+	var cleanup = [];
+	for (var i = 0; i < clientList.length;  i+=1) {
+		if (clientList[i] !== client) {
+			if (clientList[i].writable) {
+				clientList[i].write(client.name + " says " + message + '\n');
+				
+			} else {
+				cleanup.push(clientList[i]);
+				clientList[i].destory();
+			}
+		}
+	}
 
 }
-
-);
 
 chatServer.listen(9000);
